2cea2271e5b63d27dc417050d47ee880d2aaee05
happlesful <Swislar1108@gmail.com>
2024-06-27 23:06:35 +0800

    Added: Statistics page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,6 +2,8 @@ export { default as Admin } from "./Admin";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { default as Home } from "./Home";
+export { default as About } from "./About.tsx";
+export { default as Statistics } from "./Statistics.tsx";
 export { default as Main } from "./Main";
 export { default as TTT } from "./TTT";
 export { default as TZFE } from "./TZFE";
