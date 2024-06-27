16be7e2357aca996a7a33ed94dd3942e9f7d6a05
happlesful <Swislar1108@gmail.com>
2024-06-27 22:42:07 +0800

    Added: Secondary navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../context/authContext";
 import { Moon, Sun } from "react-feather";
 import { motion, AnimatePresence } from "framer-motion";
 
-const Navbar = () => {
+const Navbar = ({ pageName = "" }) => {
   const { darkMode, setDarkMode } = useContext(AuthContext);
   const { currentUser, logout } = useContext(AuthContext);
   const location = useLocation();
@@ -26,47 +26,73 @@ const Navbar = () => {
   };
 
   return (
-    <div className="absolute top-0 left-0 w-full h-10 flex justify-between navbar">
-      <Link
-        to={location.pathname === "/admin" ? "/" : "/home"}
-        className="flex items-center pl-3 justify-center text-xl"
-      >
-        Game Center
-      </Link>
-      <span className="relative flex flex-row py-2 mr-3 top-0 right-0 items-center justify-center">
-        <p className="pr-3 cursor-default">
-          {currentUser ? currentUser.username : "Login →"}
-        </p>
-        <Link to="/">
-          <LogOut
-            className="mr-4 scale-150 hover:cursor-pointer ease-in duration-100 p-1 rounded-md navbar-button"
-            onClick={logout}
-          />
+    <div className="absolute top-0 left-0 w-full flex flex-col">
+      <section className="h-10 flex justify-between navbar">
+        <Link
+          to={location.pathname === "/admin" ? "/" : "/home"}
+          className="flex items-center pl-3 justify-center text-xl"
+        >
+          Game Center
         </Link>
-        <AnimatePresence mode="wait">
-          <motion.div
-            initial={{ opacity: 0, rotate: 25, scale: 1.05 }}
-            animate={{ opacity: 1, rotate: 0, scale: 1 }}
-            exit={{ opacity: 0, rotate: 25, scale: 1.05 }}
-            transition={{ duration: 1 }}
-            key={darkMode ? "moon" : "sun"}
-          >
-            {darkMode ? (
-              <Moon
-                className="hover:cursor-pointer fill-slate-400"
-                onClick={() => toggleDarkMode()}
-              />
-            ) : (
-              <Sun
-                className="hover:cursor-pointer fill-amber-400"
-                onClick={() => {
-                  toggleDarkMode();
-                }}
-              />
-            )}
-          </motion.div>
-        </AnimatePresence>
-      </span>
+        <span className="relative flex flex-row py-2 mr-3 top-0 right-0 items-center justify-center">
+          <p className="pr-3 cursor-default">
+            {currentUser ? currentUser.username : "Login →"}
+          </p>
+          <Link to="/">
+            <LogOut
+              className="mr-4 scale-150 hover:cursor-pointer ease-in duration-100 p-1 rounded-md navbar-button"
+              onClick={logout}
+            />
+          </Link>
+          <AnimatePresence mode="wait">
+            <motion.div
+              initial={{ opacity: 0, rotate: 25, scale: 1.05 }}
+              animate={{ opacity: 1, rotate: 0, scale: 1 }}
+              exit={{ opacity: 0, rotate: 25, scale: 1.05 }}
+              transition={{ duration: 1 }}
+              key={darkMode ? "moon" : "sun"}
+            >
+              {darkMode ? (
+                <Moon
+                  className="hover:cursor-pointer fill-slate-400"
+                  onClick={() => toggleDarkMode()}
+                />
+              ) : (
+                <Sun
+                  className="hover:cursor-pointer fill-amber-400"
+                  onClick={() => {
+                    toggleDarkMode();
+                  }}
+                />
+              )}
+            </motion.div>
+          </AnimatePresence>
+        </span>
+      </section>
+      <section className="h-7 w-full flex flex-row justify-evenly items-center">
+        <Link
+          to="/home"
+          className={`menu-button w-[50%] text-center border-2 ease-in-out duration-300 hover:cursor-pointer ${
+            pageName === "Home"
+              ? "border-cyan-300 border-4"
+              : pageName === ""
+              ? "border-cyan-200"
+              : "border-cyan-200 border-r-0"
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={`menu-button w-[50%] text-center border-2 ease-in-out duration-300 hover:cursor-pointer ${
+            pageName === "About"
+              ? "border-cyan-300 border-4"
+              : "border-cyan-200 border-l-0"
+          }`}
+        >
+          About
+        </Link>
+      </section>
     </div>
   );
 };
