ce848f1f12c3bea5333a6ad5f2838e503aa5c055
happlesful <Swislar1108@gmail.com>
2024-06-29 15:26:54 +0800

    Fixed: Airplane component typo

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,7 +3,7 @@ export { default as TicTacToe } from "./TicTacToe";
 export { default as TZFEGameboard } from "./TZFEGameboard";
 export { default as TwoZeroFourEight } from "./TwoZeroFourEight";
 export { default as AirplaneGameBoard } from "./AirplaneGameBoard";
-export { default as Airplane } from "./Airplane";
+export { default as Airplane } from "./Airplane.jsx";
 export { default as Games } from "./Games";
 export { default as Navbar } from "./Navbar";
 export { default as LoadingSpinner } from "./LoadingSpinner";
