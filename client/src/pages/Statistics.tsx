a9f535189f562a08756d95ce9398d6fc97c73398
happlesful <Swislar1108@gmail.com>
2024-06-29 15:25:19 +0800

    Update: Animate history log on load

--- client/src/pages/Statistics.tsx
+++ client/src/pages/Statistics.tsx
@@ -24,12 +24,20 @@ interface GameStatistics {
   highscore: number;
 }
 
+interface GameHistory {
+  game: string;
+  category: number;
+  score: number;
+  date: string;
+}
+
 const Statistics = () => {
   const [screenSize, setScreenSize] = useState(window.innerWidth < 768 ? 0 : 1); //0 for mobile, 1 for desktop
   const [userSearch, setUserSearch] = useState("");
   const [currentUser, setCurrentUser] = useState("");
   const [userData, setUserData] = useState([]);
   const [userStatistics, setUserStatistics] = useState([]);
+  const [userHistory, setUserHistory] = useState([]);
   const [loading, setLoading] = useState(true);
   const { currentDomain, darkMode } = useContext(AuthContext);
 
@@ -41,6 +49,7 @@ const Statistics = () => {
     setUserSearch(username);
     setCurrentUser(username);
     getUserStatistics(userId);
+    getUserHistory(userId);
   };
 
   const getUsers = async () => {
@@ -68,6 +77,19 @@ const Statistics = () => {
     }
   };
 
+  const getUserHistory = async (userId: string) => {
+    try {
+      const res = await axios.get(
+        `${currentDomain}/api/users/user-history/${userId}`
+      );
+      setUserHistory(res.data);
+      console.log(res.data);
+    } catch (err) {
+      setUserHistory([]);
+      console.log(err);
+    }
+  };
+
   const renderGameTitle = (gameCode: string) => {
     switch (gameCode) {
       case "tzfe":
@@ -75,6 +97,18 @@ const Statistics = () => {
       case "ttt":
         return "Tick Tac Toe";
     }
+  };
+
+  const historyVariant = {
+    initial: {
+      opacity: 0,
+    },
+    animate: { opacity: 1, transition: { staggerChildren: 0.2 } },
+  };
+
+  const historyChildVariant = {
+    initial: { opacity: 0, y: 10 },
+    animate: { opacity: 1, y: 0 },
   };
 
   useEffect(() => {
@@ -96,7 +130,7 @@ const Statistics = () => {
   }, [screenSize]);
 
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-full overflow-y-scroll sm:overflow-y-auto">
       <Navbar pageName={"Statistics"} />
       {loading && <LoadingSpinner />}
       {!loading && (
@@ -129,53 +163,125 @@ const Statistics = () => {
                 ))}
             </ul>
           </motion.div>
-          <div
-            className={`flex flex-col font-mono pt-3 sm:pt-0 ${
-              darkMode ? "text-auto border-auto" : "text-neutral-950"
-            }`}
-          >
-            {currentUser === "" ? (
-              ""
-            ) : (
-              <h1
-                className={`text-center border-b-2 mb-2 ease-in-out duration-500 ${
-                  darkMode ? "border-slate-200" : "border-black"
-                }`}
-              >
-                Data for {currentUser}
-              </h1>
-            )}
-            {currentUser === "" ? (
-              ""
-            ) : userStatistics.length !== 0 ? (
-              userStatistics.map((gameStats: GameStatistics) => (
+          <div className="flex flex-col items-center justify-center">
+            <div
+              className={`flex flex-col font-mono pt-3 sm:pt-0 ${
+                darkMode ? "text-auto border-auto" : "text-neutral-950"
+              }`}
+            >
+              {currentUser === "" ? (
+                ""
+              ) : (
+                <h1
+                  className={`text-center border-b-2 mb-2 ease-in-out duration-500 ${
+                    darkMode ? "border-slate-200" : "border-black"
+                  }`}
+                >
+                  Data for {currentUser}
+                </h1>
+              )}
+              {currentUser === "" ? (
+                ""
+              ) : userStatistics.length !== 0 ? (
+                userStatistics.map((gameStats: GameStatistics) => (
+                  <div>
+                    <h2
+                      className={`text-center bg-opacity-30 px-2 duration-500 ease-in-out ${
+                        darkMode ? "bg-cyan-200" : "bg-stone-800"
+                      }`}
+                    >
+                      {renderGameTitle(gameStats.game)} - {gameStats.category}x
+                      {gameStats.category}
+                    </h2>
+                    <p className="text-center ease-in-out duration-500">
+                      {gameStats.game === "tzfe"
+                        ? "High-score"
+                        : gameStats.game === "ttt"
+                        ? "Plays"
+                        : ""}
+                      {": "}
+                      {gameStats.game === "tzfe"
+                        ? gameStats.highscore
+                        : gameStats.game === "ttt"
+                        ? gameStats.plays
+                        : ""}
+                    </p>
+                  </div>
+                ))
+              ) : (
+                <p className="text-center">No data found!</p>
+              )}
+            </div>
+            <div
+              className={`relative flex flex-col font-mono pt-3 pb-16 ${
+                darkMode ? "text-auto border-auto" : "text-neutral-950"
+              }`}
+            >
+              {currentUser === "" ? (
+                ""
+              ) : (
+                <h1
+                  className={`text-center border-b-2 mb-2 ease-in-out duration-500 ${
+                    darkMode ? "border-slate-200" : "border-black"
+                  }`}
+                >
+                  History
+                </h1>
+              )}
+              {currentUser === "" ? (
+                ""
+              ) : (
                 <div>
-                  <h2
-                    className={`text-center bg-opacity-30 px-2 duration-500 ease-in-out ${
-                      darkMode ? "bg-cyan-200" : "bg-stone-800"
-                    }`}
+                  <span className="flex flex-row items-center justify-evenly">
+                    <p>Game</p>
+                    <p>Score</p>
+                    <p>Date</p>
+                  </span>
+                  <motion.span
+                    variants={historyVariant}
+                    initial="initial"
+                    animate="animate"
+                    transition={{ duration: 0.1 }}
+                    key={currentUser}
                   >
-                    {renderGameTitle(gameStats.game)} - {gameStats.category}x
-                    {gameStats.category}
-                  </h2>
-                  <p className="text-center ease-in-out duration-500">
-                    {gameStats.game === "tzfe"
-                      ? "High-score"
-                      : gameStats.game === "ttt"
-                      ? "Plays"
-                      : ""}
-                    {": "}
-                    {gameStats.game === "tzfe"
-                      ? gameStats.highscore
-                      : gameStats.game === "ttt"
-                      ? gameStats.plays
-                      : ""}
-                  </p>
+                    {userHistory.length !== 0 ? (
+                      userHistory
+                        .filter(
+                          (gameHistory: GameHistory) =>
+                            gameHistory.game === "tzfe"
+                        )
+                        .map((gameHistory: GameHistory, id) => (
+                          <motion.p
+                            variants={historyChildVariant}
+                            key={currentUser + id}
+                            className={`flex flex-row justify-between items-center text-center text-sm bg-opacity-30 px-2 duration-500 ease-in-out ${
+                              darkMode
+                                ? id % 2
+                                  ? "bg-cyan-100"
+                                  : "bg-cyan-600"
+                                : id % 2
+                                ? "bg-stone-500"
+                                : "bg-stone-950"
+                            }`}
+                          >
+                            <p className="px-1">
+                              {renderGameTitle(gameHistory.game)}&#40;
+                              {gameHistory.category}x{gameHistory.category}
+                              &#41;
+                            </p>
+                            |<p className="px-1">{gameHistory.score}</p>|
+                            <p className="px-1">
+                              {gameHistory.date.slice(0, 10)}
+                            </p>
+                          </motion.p>
+                        ))
+                    ) : (
+                      <p className="text-center">No data found!</p>
+                    )}
+                  </motion.span>
                 </div>
-              ))
-            ) : (
-              <p className="text-center">No data found!</p>
-            )}
+              )}
+            </div>
           </div>
         </section>
       )}
